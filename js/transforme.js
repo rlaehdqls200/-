7903f946f7d2454fe3a2fb1fd3bd3a1c7eafc564
rlaehdqls200 <rlaehdqls200@gmail.com>
2023-06-14 22:09:23 +0900

    모바일 환경에서 웹 환경이 잘못 출력되는 js수정

--- js/transforme.js
+++ js/transforme.js
@@ -1,36 +1,47 @@
-//로드 시 기준으로 적용되는 스크립트
 window.addEventListener("load", handleLoad);
-  
+
 function handleLoad() {
-    var container = document.getElementsByClassName("container")[0];
-    var container2 = document.getElementsByClassName("container2")[0];
-    var width = window.innerWidth;
-    //화면넓이가 768보다 클시 style.css랑 HTML에 container2안에 내용 활성화
-    if  (width >= 768) {
-        loadStyleSheet("/css/test.css");
-        loadStyleSheet("/css/reset.css");
-        unloadStyleSheet("/css/style_app.css");
-        container.style.display = "none";
-        container2.style.display = "block";
-    //화면넓이가 768보다 작을시 style_app.css(앱 전용 화면)랑 HTML에 container안에 내용 활성화
-    } else {
-      loadStyleSheet("/css/style_app.css");
-        unloadStyleSheet("/css/reset.css");
-        unloadStyleSheet("/css/test.css");
-        container.style.display = "block";
-        container2.style.display = "none";
-    }
+  applyStyle();
+
+  // 초기 로드 후에 resize 이벤트 제거
+  window.removeEventListener("resize", handleResize);
 }
 
-function loadStyleSheet(stylesheet) {
+function handleResize() {
+  applyStyle();
+}
+
+function applyStyle() {
+  var container = document.getElementsByClassName("container")[0];
+  var container2 = document.getElementsByClassName("container2")[0];
+  var width = document.documentElement.clientWidth;
+  if (width <= 768) {
+  //width가 768 보다 작을 시 모바일 환경
+    loadStyleSheet("/css/style_app.css");
+    unloadStyleSheet("/css/reset.css");
+    unloadStyleSheet("/css/test.css");
+    container.style.display = "block";
+    container2.style.display = "none";
+  } else {
+  //width가 768 보다 클 시 웹 환경
+    loadStyleSheet("/css/test.css");
+    loadStyleSheet("/css/reset.css");
+    unloadStyleSheet("/css/style_app.css");
+    container.style.display = "none";
+    container2.style.display = "block";
+  }
+}
+
+function loadStyleSheet(url) {
   var link = document.createElement("link");
   link.rel = "stylesheet";
-  link.href = stylesheet;
+  link.href = url;
   document.head.appendChild(link);
 }
-function unloadStyleSheet(stylesheet) {
-  var stylesheets = document.querySelectorAll('link[rel="stylesheet"][href="' + stylesheet + '"]');
-  stylesheets.forEach(function (stylesheet) {
-      stylesheet.parentNode.removeChild(stylesheet);
-  });
+
+function unloadStyleSheet(url) {
+  var stylesheets = document.querySelectorAll('link[rel="stylesheet"][href="' + url + '"]');
+  for (var i = 0; i < stylesheets.length; i++) {
+    stylesheets[i].parentNode.removeChild(stylesheets[i]);
+  }
 }
